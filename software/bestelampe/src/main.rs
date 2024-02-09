a3f464e34a63db61ea074c1ce814c76ab6b67e2b
Lena Schimmel <mail@lenaschimmel.de>
2024-02-09 12:43:07 +0100

    Refactor wifi and server functions: less code duplication, better separation of concerns, more ideomatic code.

--- software/bestelampe/src/main.rs
+++ software/bestelampe/src/main.rs
@@ -74,7 +74,8 @@ fn main() -> ! {
     });
 
     let _wifi_thread = thread::spawn(|| {
-        test_wifi(peripherals.modem, light_temperature_target, light_brightness_target, true);
+        start_wifi(peripherals.modem, true).unwrap();
+        run_server(light_temperature_target, light_brightness_target).unwrap();
     });
 
     println!("Entering infinite loop in main thread...");
@@ -163,15 +164,11 @@ fn test_leds(
         driver_5,
     )?;
     
-    let mut target_brightness = 0.0;
-    let mut target_temperature = 0.0;
-    {
-        target_brightness = *(light_brightness_target.read().unwrap());
-        target_temperature = *(light_temperature_target.read().unwrap());
-    }
-    let mut brightness = target_brightness;
-    let mut temperature = target_temperature;
-
+    let (mut target_brightness, mut target_temperature) = {(
+        *(light_brightness_target.read().unwrap()),
+        *(light_temperature_target.read().unwrap())
+    )};
+    let (mut brightness, mut temperature) = (target_brightness, target_temperature);
     let lerp_speed = 0.01;
 
     loop {
@@ -261,20 +258,16 @@ struct FormData {
     temperature: f32,
 }
 
-fn test_wifi(
-    modem: Modem,
+fn run_server(
     light_temperature_target: Arc<RwLock<f32>>,
     light_brightness_target: Arc<RwLock<f32>>,
-    as_access_point: bool
 ) -> Result<()> {
-    // this used to be the main method of an example program
-
-    let mut server: EspHttpServer<'_>;
-    if (as_access_point) {
-        server = create_access_point_server(modem)?;
-    } else {
-        server = create_station_server(modem)?;
-    }
+    
+    let server_configuration = esp_idf_svc::http::server::Configuration {
+        stack_size: STACK_SIZE,
+        ..Default::default()
+    };
+    let mut server: EspHttpServer<'_> = EspHttpServer::new(&server_configuration).or(Err(anyhow!("Could not create server.")))?;
     println!("Created the server. Attaching handlers.");
 
     server.fn_handler::<anyhow::Error, _>("/", Method::Get, |req| {
@@ -313,23 +306,10 @@ fn test_wifi(
 
     println!("Handlers attached.");
 
-
     loop {
         println!("Inside server keep-alive loop.");
         std::thread::sleep(core::time::Duration::from_millis(2000));
     }
-
-    println!("The server is over now.");
-
-    // Keep server running beyond when main() returns (forever)
-    // Do not call this if you ever want to stop or access it later.
-    // Otherwise you can either add an infinite loop so the main task
-    // never returns, or you can move it to another thread.
-    // https://doc.rust-lang.org/stable/core/mem/fn.forget.html
-    core::mem::forget(server);
-
-    // Main task no longer needed, free up some memory
-    Ok(())
 }
 
 #[toml_cfg::toml_config]
@@ -351,8 +331,11 @@ const STACK_SIZE: usize = 10240;
 // Wi-Fi channel, between 1 and 11
 const CHANNEL: u8 = 11;
 
-fn create_station_server(modem: Modem) -> Result<EspHttpServer<'static>> {
-    println!("Inside 'create_station_server'...");
+/// Starts the wifi either as station ("client") or access point.
+/// Does not have any retry-loop or error handling.
+/// Method returns when the wifi is ready to be used.
+fn start_wifi(modem: Modem, as_access_point: bool) -> Result<()> {
+    println!("Inside 'start_wifi'...");
     let sys_loop = EspSystemEventLoop::take()?;
     let nvs = EspDefaultNvsPartition::take()?;
 
@@ -361,66 +344,37 @@ fn create_station_server(modem: Modem) -> Result<EspHttpServer<'static>> {
         sys_loop,
     )?;
 
-    let wifi_configuration = esp_idf_svc::wifi::Configuration::Client(esp_idf_svc::wifi::ClientConfiguration {
-        ssid: CONFIG.wifi_ssid.try_into().or(Err(anyhow!("Invalid SSID config.")))?,
-        password: CONFIG.wifi_psk.try_into().or(Err(anyhow!("Invalid PSK config.")))?,
-        auth_method: AuthMethod::WPA2Personal,
-        ..Default::default()
-    });
+    let wifi_configuration = match as_access_point {
+        false => 
+            esp_idf_svc::wifi::Configuration::Client(esp_idf_svc::wifi::ClientConfiguration {
+            ssid: CONFIG.wifi_ssid.try_into().or(Err(anyhow!("Invalid SSID config.")))?,
+            password: CONFIG.wifi_psk.try_into().or(Err(anyhow!("Invalid PSK config.")))?,
+            auth_method: AuthMethod::WPA2Personal,
+            ..Default::default()
+        }),
+        true => esp_idf_svc::wifi::Configuration::AccessPoint(esp_idf_svc::wifi::AccessPointConfiguration {
+            ssid: CONFIG.wifi_ssid.try_into().or(Err(anyhow!("Invalid SSID config.")))?,
+            password: CONFIG.wifi_psk.try_into().or(Err(anyhow!("Invalid PSK config.")))?,
+            ssid_hidden: false,
+            auth_method: AuthMethod::WPA2Personal,
+            channel: CHANNEL,
+            ..Default::default()
+        })
+    };
+
     wifi.set_configuration(&wifi_configuration)?;
     wifi.start()?;
-    wifi.connect()?;
+    if !as_access_point {
+        wifi.connect()?;
+    }
     wifi.wait_netif_up()?;
 
     info!(
-        "Joined Wi-Fi with WIFI_SSID `{}` and WIFI_PASS `{}`",
-        CONFIG.wifi_ssid, CONFIG.wifi_psk
+        "Joined Wi-Fi with WIFI_SSID `{}` and WIFI_PASS `{}` as {}",
+        CONFIG.wifi_ssid, CONFIG.wifi_psk, if as_access_point { "access point" } else { "station" }
     );
-
-    let server_configuration = esp_idf_svc::http::server::Configuration {
-        stack_size: STACK_SIZE,
-        ..Default::default()
-    };
 
     core::mem::forget(wifi);
 
-    return EspHttpServer::new(&server_configuration).or(Err(anyhow!("Could not create server.")));
-}
-
-fn create_access_point_server(modem: Modem) -> Result<EspHttpServer<'static>> {
-    println!("Inside 'create_access_point_server'...");
-    let sys_loop = EspSystemEventLoop::take()?;
-    let nvs = EspDefaultNvsPartition::take()?;
-
-    let mut wifi = BlockingWifi::wrap(
-        EspWifi::new(modem, sys_loop.clone(), Some(nvs))?,
-        sys_loop,
-    )?;
-
-    let wifi_configuration = esp_idf_svc::wifi::Configuration::AccessPoint(esp_idf_svc::wifi::AccessPointConfiguration {
-        ssid: CONFIG.wifi_ssid.try_into().or(Err(anyhow!("Invalid SSID config.")))?,
-        password: CONFIG.wifi_psk.try_into().or(Err(anyhow!("Invalid PSK config.")))?,
-        ssid_hidden: false,
-        auth_method: AuthMethod::WPA2Personal,
-        channel: CHANNEL,
-        ..Default::default()
-    });
-    wifi.set_configuration(&wifi_configuration)?;
-    wifi.start()?;
-    wifi.wait_netif_up()?;
-
-    info!(
-        // This will output the configuration as it is read, e.g. currently empty strings.
-        "Created Wi-Fi with WIFI_SSID `{}` and WIFI_PASS `{}`",
-        CONFIG.wifi_ssid, CONFIG.wifi_psk
-    );
-
-    let server_configuration = esp_idf_svc::http::server::Configuration {
-        stack_size: STACK_SIZE,
-        ..Default::default()
-    };
-
-    core::mem::forget(wifi);
-
-    return EspHttpServer::new(&server_configuration).or(Err(anyhow!("Could not create server.")));
+    return Ok(());
 }
